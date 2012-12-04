fa3dcc7f187b3d056fad113c961a6d8c824815f7
Graham Gilbert <graham@grahamgilbert.com>
2012-12-04 17:25:14 +0000

    More bugfixes

--- Webapp/fvserver/server/views.py
+++ Webapp/fvserver/server/views.py
@@ -31,6 +31,6 @@ def checkin(request):
     computer = Computer(recovery_key=recovery_pass, serial=serial_num, last_checkin = datetime.now())
     computer.save()
     
-    c ={'revovery_password':computer.recovery_key, 'serial':computer.serial, 'domain':group.domain, }
+    c ={'revovery_password':computer.recovery_key, 'serial':computer.serial, }
     return HttpResponse(simplejson.dumps(c), mimetype="application/json")
         
